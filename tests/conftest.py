7170f94c25414d4d80801508b3584ac0ecf69eed
Daria Jakuszewicz <d.jakuszewicz@gmail.com>
2024-03-30 13:45:47 +0300

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,15 @@ import os
 import pytest
 import shutil
 
-CURRENT_FILE = os.path.abspath(__file__)  # абсолютный путь к текущему файлу
-PROJECT_DIR = os.path.dirname(os.path.dirname(CURRENT_FILE))  # путь к директории где находится файл с которым работаем
-TEMP_DIR = os.path.join(PROJECT_DIR, 'temp')  # делаем склейку пути к текущей директории и папке tmp
-RESOURCE_DIR = os.path.join(PROJECT_DIR, 'resource')  # делаем склейку пути к директории RESOURCE и папке 'resource'
+from paths import RESOURCE_DIR, TEMP_DIR
 
 @pytest.fixture(scope='session', autouse=True)
 def create_archive():
-    if not os.path.exists(RESOURCE_DIR):  # проверяем существует ли папка
-        os.mkdir(RESOURCE_DIR)  # создаем папку если её нет
-    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip'),'w') as zf:  # создаем архив
-        for file in os.listdir(TEMP_DIR):  # добавляем файлы в архив
-            add_file = os.path.join(TEMP_DIR, file)  # склеиваем путь к файлам которые добавляют в архив
-            zf.write(add_file, os.path.basename(add_file))  # добавляем файл в архив
+    if not os.path.exists(RESOURCE_DIR):
+        os.mkdir(RESOURCE_DIR)
+    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip'),'w') as zf:
+        for file in os.listdir(TEMP_DIR):
+            add_file = os.path.join(TEMP_DIR, file)
+            zf.write(add_file, os.path.basename(add_file))
     yield
-    shutil.rmtree(RESOURCE_DIR)  # удаляем файлы после архивации
+    shutil.rmtree(RESOURCE_DIR)
--- tests/test_zip.py
+++ tests/test_zip.py
@@ -3,36 +3,33 @@ import os
 from pypdf import PdfReader
 import zipfile
 from openpyxl import load_workbook
-from conftest import RESOURCE_DIR
+from paths import RESOURCE_DIR
 import csv
 
 
 def test_csv():
-    third_row = None
-    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file: # открываем архив
-        with zip_file.open('schedule.csv') as csv_file: # открываем файл в архиве
-            content = csv_file.read().decode('utf-8-sig')  # читаем содержимое файла и декодируем его если в файле есть символы не из английского алфавита
-            csvreader = list(csv.reader(content.splitlines()))  # читаем содержимое файла и преобразуем его в список
-            third_row = csvreader[2]  # получаем третью строку
+    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file:
+        with zip_file.open('schedule.csv') as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            csvreader = list(csv.reader(content.splitlines()))
+            third_row = csvreader[2]
 
-    assert third_row[0] == 'школа'  # проверка значения элемента в первом столбце третьей строки
-    assert third_row[5] == 'выходной'  # проверка значения элемента в седьмом столбце третьей строки
+    assert third_row[0] == 'школа'
+    assert third_row[5] == 'выходной'
 
 def test_pdf():
-    text = None
-    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file: # открываем архив
-        with zip_file.open('Колобок.pdf') as pdf_file:  # открываем файл в архиве
+    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file:
+        with zip_file.open('Колобок.pdf') as pdf_file:
             reader = PdfReader(pdf_file)
-            page = reader.pages[0]  # получаем первую страницу
-            text = page.extract_text()  # извлекаем текст из первой страницы
+            page = reader.pages[0]
+            text = page.extract_text()
 
     assert text.find('Автор: Русская народная') != -1
 
 def test_xlsx():
-    sheet = None
-    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file:  # открываем архив
-        with zip_file.open('pers.xlsx') as xlsx_file:  # открываем файл в архиве
-            workbook = load_workbook(xlsx_file)  # открываем файл
-            sheet = workbook.active  # получаем активный лист
+    with zipfile.ZipFile(os.path.join(RESOURCE_DIR, 'files.zip')) as zip_file:
+        with zip_file.open('pers.xlsx') as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
 
     assert sheet.cell(row=2, column=8).value == '88002000600'
